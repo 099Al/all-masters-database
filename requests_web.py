b70d217cac098f02c0821662851ee98e62b9c7b8
efimov_as <efimov_as@tochka.com>
2025-09-01 00:01:35 +0500

    web request

---
+++ requests_web.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+from src.database.connect import DataBase
+from src.database.models import Specialist, UserStatus
+
+
+class ReqWeb:
+    def __init__(self):
+        self.session = DataBase().get_session()
+
+    async def get_active_specialists_data(self):
+        async with self.session() as session:
+            result = await session.execute(
+                select(
+                    Specialist.name,
+                    Specialist.email,
+                    Specialist.phone,
+                    Specialist.photo_local,
+                    Specialist.photo_telegram,
+                    Specialist.services,
+                    Specialist.about
+                )
+                .where(Specialist.status == UserStatus.ACTIVE)
+            )
+            res = result.all()
+
+        return res
