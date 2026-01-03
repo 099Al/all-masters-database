15c8482dc26ff298d8cd062f6499a645dafc9b44
efimov_as <efimov_as@tochka.com>
2026-01-03 20:37:28 +0500

    service added category_ids

--- models/models.py
+++ models/models.py
@@ -55,7 +55,7 @@ class Specialist(Base):
     created_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
     updated_at: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
     ban_reason: Mapped[str] = mapped_column(String(300), nullable=True)
-    l_services: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
+    l_services: Mapped[List[str]] = mapped_column(JSONB, nullable=True)   #не учитывает категорию. Пример: электрика может быть в разных категориях
     l_service_ids: Mapped[List[int]] = mapped_column(JSONB, nullable=True)
     l_work_types: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
 
@@ -144,6 +144,7 @@ class SpecialistService(Base):
     __tablename__ = "specialist_services"
 
     specialist_id = Column(Integer, ForeignKey("specialists.id"), primary_key=True)
+    category_id = Column(Integer, ForeignKey("categories.id"), primary_key=True)
     service_id = Column(Integer, ForeignKey("services.id"), primary_key=True)
 
     def __repr__(self):
--- sql/4_update_statuses_proc.sql
+++ sql/4_update_statuses_proc.sql
@@ -108,21 +108,22 @@ UPDATE SET
 
 -- перенос услуг (линков)
 create temporary table tmp_moved on commit drop as
-    select ms.specialist_id, ms.service_id
+    select ms.specialist_id, ms.service_id, s.category_id
     from moderatedata_services ms
     join services s on s.id = ms.service_id
     join categories c on c.id = s.category_id
     where c.is_new = false
       and s.is_new = false;
 
-insert into specialist_services (specialist_id, service_id)
-select specialist_id, service_id
+insert into specialist_services (specialist_id, category_id, service_id)
+select specialist_id, category_id, service_id
 from tmp_moved;
 
 delete from moderatedata_services ms
 using tmp_moved t
 where ms.specialist_id = t.specialist_id
-  and ms.service_id = t.service_id;
+  and ms.service_id = t.service_id
+  and ms.category_id = t.category_id;
 
 
 --перенос фото
